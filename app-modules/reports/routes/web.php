ed7ad91cb636549fd0aa768288cfc3d8932e5e9f
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 14:28:15 +0330

    Apply web middleware to report routes

--- app-modules/reports/routes/web.php
+++ app-modules/reports/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Reports\Presentation\Http\Controllers\ReportController;
 
 Route::get('/reports', ReportController::class)
-    ->middleware(['auth', 'permission:reports.view'])
+    ->middleware(['web', 'auth', 'permission:reports.view'])
     ->name('reports.index');
