1b7ce0c94ce694b2ce056928dc91eb060803513d
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 16:59:18 +0330

    Add HTTP task attachment download boundary

---
+++ app-modules/tasks/src/Presentation/Http/Controllers/TaskAttachmentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Modules\Tasks\Presentation\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Routing\Controller;
+use Modules\Tasks\Application\Queries\TaskAccessScope;
+use Modules\Tasks\Domain\Contracts\TaskAttachmentReader;
+use Modules\Tasks\Domain\Contracts\TaskRepository;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class TaskAttachmentController extends Controller
+{
+    public function __construct(
+        private readonly TaskRepository $tasks,
+        private readonly TaskAccessScope $scopeBuilder,
+        private readonly TaskAttachmentReader $attachmentReader,
+    ) {}
+
+    public function __invoke(int $task, int $media): BinaryFileResponse
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        $scope = $this->scopeBuilder->for($user);
+
+        $this->tasks->findAccessible($task, $scope);
+        $file = $this->attachmentReader->get($task, $media);
+
+        return response()->download(
+            $file['path'],
+            $file['name'],
+            ['Content-Type' => $file['mime_type'] ?? 'application/octet-stream'],
+        );
+    }
+}
