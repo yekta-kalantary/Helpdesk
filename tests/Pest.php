e3e558db62b9dc89317a127a2722f842898d6b53
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 14:25:05 +0330

    Disable Vite asset resolution in feature tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,6 +7,7 @@ use Tests\TestCase;
 pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
     ->beforeEach(function (): void {
+        $this->withoutVite();
         $this->seed(DatabaseSeeder::class);
     })
     ->in('Feature');
