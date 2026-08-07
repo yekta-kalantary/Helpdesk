fa9cf2fc63ba939f748a977963a837ac3124764a
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 12:56:12 +0330

    register tickets module

---
+++ app-modules/tickets/src/TicketsServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Tickets;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Tickets\Domain\Contracts\TicketAttachmentStore;
+use Modules\Tickets\Domain\Contracts\TicketNotifier;
+use Modules\Tickets\Domain\Contracts\TicketRepository;
+use Modules\Tickets\Infrastructure\DatabaseTicketNotifier;
+use Modules\Tickets\Infrastructure\EloquentTicketRepository;
+use Modules\Tickets\Infrastructure\LocalTicketAttachmentStore;
+
+class TicketsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(TicketRepository::class, EloquentTicketRepository::class);
+        $this->app->bind(TicketAttachmentStore::class, LocalTicketAttachmentStore::class);
+        $this->app->bind(TicketNotifier::class, DatabaseTicketNotifier::class);
+    }
+
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'tickets');
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'tickets');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+}
