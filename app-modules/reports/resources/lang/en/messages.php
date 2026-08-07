ceee71e9ed4d335913a3c5e0c37f0fab1f4f0166
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 13:14:31 +0330

    add english report translations

---
+++ app-modules/reports/resources/lang/en/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'reports' => 'Reports',
+    'customers' => 'Customer report',
+    'projects' => 'Project report',
+    'team' => 'Team report',
+    'company' => 'Company',
+    'projects_count' => 'Projects',
+    'open_tickets' => 'Open tickets',
+    'tasks_count' => 'Tasks',
+    'done_tasks' => 'Completed tasks',
+    'overdue_tasks' => 'Overdue tasks',
+    'progress' => 'Progress',
+    'assigned_tasks' => 'Assigned tasks',
+];
