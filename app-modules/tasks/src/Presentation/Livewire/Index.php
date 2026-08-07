1232d3080f424a6a17f06dfa81764e28febb305e
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 16:56:59 +0330

    Add Livewire task index

---
+++ app-modules/tasks/src/Presentation/Livewire/Index.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Tasks\Presentation\Livewire;
+
+use App\Models\User;
+use Livewire\Attributes\Url;
+use Livewire\Component;
+use Modules\Tasks\Application\Queries\TaskAccessScope;
+use Modules\Tasks\Domain\Contracts\TaskRepository;
+use Modules\Tasks\Domain\Enums\TaskStatus;
+
+class Index extends Component
+{
+    #[Url(as: 'q', except: '')]
+    public string $q = '';
+
+    #[Url(as: 'project', except: null)]
+    public ?int $projectId = null;
+
+    protected TaskRepository $tasks;
+
+    protected TaskAccessScope $scopeBuilder;
+
+    public function boot(TaskRepository $tasks, TaskAccessScope $scopeBuilder): void
+    {
+        $this->tasks = $tasks;
+        $this->scopeBuilder = $scopeBuilder;
+    }
+
+    public function render()
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        $scope = $this->scopeBuilder->for($user);
+
+        return view('tasks::index', [
+            'tasks' => $this->tasks->search($scope, $this->projectId, trim($this->q) ?: null),
+            'statuses' => TaskStatus::cases(),
+        ])->title(__('tasks::messages.tasks'));
+    }
+}
