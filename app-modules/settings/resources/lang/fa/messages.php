2f5548e29678d8f571fd8ea978d5cb3719aa4d60
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 13:05:00 +0330

    add persian settings translations

---
+++ app-modules/settings/resources/lang/fa/messages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'smtp' => 'تنظیمات SMTP',
+    'enabled' => 'ارسال ایمیل از طریق SMTP فعال باشد',
+    'host' => 'SMTP Host',
+    'port' => 'Port',
+    'username' => 'نام کاربری',
+    'password' => 'رمز عبور',
+    'password_configured' => 'رمز عبور فعلی ذخیره شده است؛ برای حفظ آن این فیلد را خالی بگذارید.',
+    'scheme' => 'Scheme',
+    'scheme.auto' => 'خودکار / STARTTLS',
+    'scheme.smtp' => 'SMTP',
+    'scheme.smtps' => 'SMTPS',
+    'from_address' => 'ایمیل فرستنده',
+    'from_name' => 'نام فرستنده',
+    'local_fallback' => 'در صورت غیرفعال بودن SMTP، ایمیل‌ها فقط در لاگ محلی ثبت می‌شوند.',
+];
