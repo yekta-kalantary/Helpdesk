927dd0fa7dde7c3a09ae451f76c4b151138e801e
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 17:03:47 +0330

    Route reports through Livewire

--- app-modules/reports/routes/web.php
+++ app-modules/reports/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Reports\Presentation\Http\Controllers\ReportController;
 
-Route::get('/reports', ReportController::class)
+Route::livewire('/reports', 'reports::index')
     ->middleware(['web', 'auth', 'permission:reports.view'])
     ->name('reports.index');
