40be49944954600dd8847c51be218a0d7fe1eb37
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 13:12:07 +0330

    add notification open flow

--- app-modules/identity/src/Presentation/Http/Controllers/NotificationController.php
+++ app-modules/identity/src/Presentation/Http/Controllers/NotificationController.php
@@ -19,6 +19,20 @@ class NotificationController extends Controller
         ]);
     }
 
+    public function open(string $notification): RedirectResponse
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        $item = $user->notifications()->findOrFail($notification);
+        $item->markAsRead();
+
+        return match ($item->data['kind'] ?? null) {
+            'task_assigned' => redirect()->route('tasks.show', $item->data['task_id']),
+            'ticket_reply' => redirect()->route('tickets.show', $item->data['ticket_id']),
+            default => redirect()->route('notifications.index'),
+        };
+    }
+
     public function markAllRead(): RedirectResponse
     {
         /** @var User $user */
