4bf097806d5673254cf50726f2494ac36e19710d
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 13:02:17 +0330

    add smtp settings model

---
+++ app-modules/settings/src/Infrastructure/Settings/SmtpSettings.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Settings\Infrastructure\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class SmtpSettings extends Settings
+{
+    public bool $enabled;
+    public ?string $host;
+    public int $port;
+    public ?string $username;
+    public ?string $password_encrypted;
+    public ?string $scheme;
+    public string $from_address;
+    public string $from_name;
+
+    public static function group(): string
+    {
+        return 'smtp';
+    }
+}
