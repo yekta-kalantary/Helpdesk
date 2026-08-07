7431f8bf0dfea386091d14ec79624fe52dcd25c3
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 16:59:33 +0330

    Route Tasks through Livewire

--- app-modules/tasks/routes/web.php
+++ app-modules/tasks/routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Tasks\Presentation\Http\Controllers\TaskController;
+use Modules\Tasks\Presentation\Http\Controllers\TaskAttachmentController;
 
 Route::middleware(['web', 'auth'])->group(function (): void {
-    Route::get('/tasks', [TaskController::class, 'index'])->middleware('permission:tasks.view')->name('tasks.index');
-    Route::get('/tasks/create', [TaskController::class, 'create'])->middleware('permission:tasks.create')->name('tasks.create');
-    Route::post('/tasks', [TaskController::class, 'store'])->middleware('permission:tasks.create')->name('tasks.store');
-    Route::get('/tasks/{task}', [TaskController::class, 'show'])->middleware('permission:tasks.view')->name('tasks.show');
-    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->middleware('permission:tasks.update')->name('tasks.edit');
-    Route::put('/tasks/{task}', [TaskController::class, 'update'])->middleware('permission:tasks.update')->name('tasks.update');
-    Route::patch('/tasks/{task}/status', [TaskController::class, 'updateStatus'])->middleware('permission:tasks.update')->name('tasks.status.update');
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->middleware('permission:tasks.delete')->name('tasks.destroy');
-    Route::post('/tasks/{task}/comments', [TaskController::class, 'comment'])->middleware('permission:tasks.comment')->name('tasks.comments.store');
-    Route::get('/tasks/{task}/attachments/{media}', [TaskController::class, 'download'])->middleware('permission:tasks.view')->name('tasks.attachments.download');
-    Route::delete('/tasks/{task}/attachments/{media}', [TaskController::class, 'deleteAttachment'])->middleware('permission:tasks.update')->name('tasks.attachments.destroy');
+    Route::livewire('/tasks', 'tasks::index')
+        ->middleware('permission:tasks.view')
+        ->name('tasks.index');
+
+    Route::livewire('/tasks/create', 'tasks::form')
+        ->middleware('permission:tasks.create')
+        ->name('tasks.create');
+
+    Route::livewire('/tasks/{task}', 'tasks::show')
+        ->middleware('permission:tasks.view')
+        ->name('tasks.show');
+
+    Route::livewire('/tasks/{task}/edit', 'tasks::form')
+        ->middleware('permission:tasks.update')
+        ->name('tasks.edit');
+
+    Route::get('/tasks/{task}/attachments/{media}', TaskAttachmentController::class)
+        ->middleware('permission:tasks.view')
+        ->name('tasks.attachments.download');
 });
