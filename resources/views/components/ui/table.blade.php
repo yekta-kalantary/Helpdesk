b1e406eb41ecc1372f480ad2f0d94b3976905644
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 16:15:20 +0330

    Use dedicated shared table hook

--- resources/views/components/ui/table.blade.php
+++ resources/views/components/ui/table.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes->class(['overflow-x-auto rounded-2xl border border-slate-200 bg-white shadow-sm']) }}>
-    <table class="table min-w-full divide-y divide-slate-200 text-sm">
+    <table class="ui-table min-w-full text-sm">
         {{ $slot }}
     </table>
 </div>
