c803a519362b65c6d7c5620bc57972b429e5f990
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 16:43:03 +0330

    Support reactive Livewire filter bars

--- resources/views/components/ui/filter-bar.blade.php
+++ resources/views/components/ui/filter-bar.blade.php
@@ -1,5 +1,11 @@
-@props(['action' => null])
+@props(['action' => null, 'livewire' => false])
 
-<form method="GET" action="{{ $action ?: url()->current() }}" {{ $attributes->class(['mb-6 flex flex-col gap-3 sm:flex-row sm:items-end']) }}>
-    {{ $slot }}
-</form>
+@if($livewire)
+    <div {{ $attributes->class(['mb-6 flex flex-col gap-3 sm:flex-row sm:items-end']) }}>
+        {{ $slot }}
+    </div>
+@else
+    <form method="GET" action="{{ $action ?: url()->current() }}" {{ $attributes->class(['mb-6 flex flex-col gap-3 sm:flex-row sm:items-end']) }}>
+        {{ $slot }}
+    </form>
+@endif
