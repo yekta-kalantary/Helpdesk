74c103b81f0b7800b633f52c9ec89b07151ccbec
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 13:10:05 +0330

    configure modular monolith conventions

---
+++ config/app-modules.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'modules_namespace' => 'Modules',
+    'modules_vendor' => null,
+    'modules_directory' => 'app-modules',
+    'tests_base' => 'Tests\\TestCase',
+    'stubs' => null,
+    'should_discover_events' => null,
+];
