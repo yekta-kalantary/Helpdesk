31df6fbc84e1e8e06f6001b819ad21bf380ea8e6
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 16:47:31 +0330

    Route customers through Livewire

--- app-modules/customers/routes/web.php
+++ app-modules/customers/routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Customers\Presentation\Http\Controllers\CustomerController;
 
 Route::middleware(['web', 'auth'])->group(function (): void {
-    Route::get('/customers', [CustomerController::class, 'index'])->middleware('permission:customers.view')->name('customers.index');
-    Route::get('/customers/create', [CustomerController::class, 'create'])->middleware('permission:customers.create')->name('customers.create');
-    Route::post('/customers', [CustomerController::class, 'store'])->middleware('permission:customers.create')->name('customers.store');
-    Route::get('/customers/{customer}/edit', [CustomerController::class, 'edit'])->middleware('permission:customers.update')->name('customers.edit');
-    Route::put('/customers/{customer}', [CustomerController::class, 'update'])->middleware('permission:customers.update')->name('customers.update');
-    Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])->middleware('permission:customers.delete')->name('customers.destroy');
+    Route::livewire('/customers', 'customers::index')
+        ->middleware('permission:customers.view')
+        ->name('customers.index');
+
+    Route::livewire('/customers/create', 'customers::form')
+        ->middleware('permission:customers.create')
+        ->name('customers.create');
+
+    Route::livewire('/customers/{customer}/edit', 'customers::form')
+        ->middleware('permission:customers.update')
+        ->name('customers.edit');
 });
