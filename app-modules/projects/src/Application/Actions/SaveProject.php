27e84e734910d8e6b27253bb8c809a338c033cda
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 12:45:11 +0330

    add project save use case

---
+++ app-modules/projects/src/Application/Actions/SaveProject.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Projects\Application\Actions;
+
+use Modules\Projects\Domain\Contracts\ProjectRepository;
+
+class SaveProject
+{
+    public function __construct(private readonly ProjectRepository $projects)
+    {
+    }
+
+    public function execute(?int $id, array $attributes, array $memberIds): int
+    {
+        if ($id) {
+            $this->projects->update($id, $attributes, $memberIds);
+            return $id;
+        }
+
+        return $this->projects->create($attributes, $memberIds);
+    }
+}
