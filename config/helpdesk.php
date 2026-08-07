0145c88f7fec8af8bcb2ff4777eabe13e225fb20
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 13:09:55 +0330

    add helpdesk installation config

---
+++ config/helpdesk.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'admin' => [
+        'name' => env('HELPDESK_ADMIN_NAME', 'Administrator'),
+        'email' => env('HELPDESK_ADMIN_EMAIL', 'admin@example.com'),
+        'password' => env('HELPDESK_ADMIN_PASSWORD', 'password'),
+    ],
+];
