a7605e58208426a446871034c8ec07965b42845c
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 14:27:49 +0330

    Apply web middleware to identity routes

--- app-modules/identity/routes/web.php
+++ app-modules/identity/routes/web.php
@@ -7,34 +7,36 @@ use Modules\Identity\Presentation\Http\Controllers\NotificationController;
 use Modules\Identity\Presentation\Http\Controllers\RoleController;
 use Modules\Identity\Presentation\Http\Controllers\UserController;
 
-Route::middleware('guest')->group(function (): void {
-    Route::get('/login', [AuthController::class, 'create'])->name('login');
-    Route::post('/login', [AuthController::class, 'store'])->name('login.store');
-});
-
-Route::middleware('auth')->group(function (): void {
-    Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
-    Route::get('/dashboard', DashboardController::class)->name('dashboard');
-
-    Route::middleware('permission:notifications.view')->group(function (): void {
-        Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
-        Route::get('/notifications/{notification}/open', [NotificationController::class, 'open'])->name('notifications.open');
-        Route::post('/notifications/read-all', [NotificationController::class, 'markAllRead'])->name('notifications.read-all');
+Route::middleware('web')->group(function (): void {
+    Route::middleware('guest')->group(function (): void {
+        Route::get('/login', [AuthController::class, 'create'])->name('login');
+        Route::post('/login', [AuthController::class, 'store'])->name('login.store');
     });
 
-    Route::get('/users', [UserController::class, 'index'])->middleware('permission:users.view')->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->middleware('permission:users.create')->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->middleware('permission:users.create')->name('users.store');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->middleware('permission:users.update')->name('users.edit');
-    Route::put('/users/{user}', [UserController::class, 'update'])->middleware('permission:users.update')->name('users.update');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('permission:users.delete')->name('users.destroy');
+    Route::middleware('auth')->group(function (): void {
+        Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
+        Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
-    Route::get('/roles', [RoleController::class, 'index'])->middleware('permission:roles.view')->name('roles.index');
-    Route::get('/roles/create', [RoleController::class, 'create'])->middleware('permission:roles.create')->name('roles.create');
-    Route::post('/roles', [RoleController::class, 'store'])->middleware('permission:roles.create')->name('roles.store');
-    Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->middleware('permission:roles.update')->name('roles.edit');
-    Route::put('/roles/{role}', [RoleController::class, 'update'])->middleware('permission:roles.update')->name('roles.update');
-    Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->middleware('permission:roles.delete')->name('roles.destroy');
-    Route::post('/permissions', [RoleController::class, 'storePermission'])->middleware('permission:roles.create')->name('permissions.store');
-    Route::delete('/permissions/{permission}', [RoleController::class, 'destroyPermission'])->middleware('permission:roles.delete')->name('permissions.destroy');
+        Route::middleware('permission:notifications.view')->group(function (): void {
+            Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
+            Route::get('/notifications/{notification}/open', [NotificationController::class, 'open'])->name('notifications.open');
+            Route::post('/notifications/read-all', [NotificationController::class, 'markAllRead'])->name('notifications.read-all');
+        });
+
+        Route::get('/users', [UserController::class, 'index'])->middleware('permission:users.view')->name('users.index');
+        Route::get('/users/create', [UserController::class, 'create'])->middleware('permission:users.create')->name('users.create');
+        Route::post('/users', [UserController::class, 'store'])->middleware('permission:users.create')->name('users.store');
+        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->middleware('permission:users.update')->name('users.edit');
+        Route::put('/users/{user}', [UserController::class, 'update'])->middleware('permission:users.update')->name('users.update');
+        Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('permission:users.delete')->name('users.destroy');
+
+        Route::get('/roles', [RoleController::class, 'index'])->middleware('permission:roles.view')->name('roles.index');
+        Route::get('/roles/create', [RoleController::class, 'create'])->middleware('permission:roles.create')->name('roles.create');
+        Route::post('/roles', [RoleController::class, 'store'])->middleware('permission:roles.create')->name('roles.store');
+        Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->middleware('permission:roles.update')->name('roles.edit');
+        Route::put('/roles/{role}', [RoleController::class, 'update'])->middleware('permission:roles.update')->name('roles.update');
+        Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->middleware('permission:roles.delete')->name('roles.destroy');
+        Route::post('/permissions', [RoleController::class, 'storePermission'])->middleware('permission:roles.create')->name('permissions.store');
+        Route::delete('/permissions/{permission}', [RoleController::class, 'destroyPermission'])->middleware('permission:roles.delete')->name('permissions.destroy');
+    });
 });
