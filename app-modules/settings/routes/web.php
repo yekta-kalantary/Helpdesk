88a2cc1d953ce424a9ac73d1e0bfea7249afb7a3
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 14:28:22 +0330

    Apply web middleware to settings routes

--- app-modules/settings/routes/web.php
+++ app-modules/settings/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Settings\Presentation\Http\Controllers\SmtpSettingsController;
 
-Route::middleware(['auth', 'permission:settings.manage'])->group(function (): void {
+Route::middleware(['web', 'auth', 'permission:settings.manage'])->group(function (): void {
     Route::get('/settings/smtp', [SmtpSettingsController::class, 'edit'])->name('settings.smtp.edit');
     Route::put('/settings/smtp', [SmtpSettingsController::class, 'update'])->name('settings.smtp.update');
 });
