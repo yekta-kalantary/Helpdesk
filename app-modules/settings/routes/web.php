5f10fec79dccbca72c1aacb7b370deeae666bd61
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 17:03:04 +0330

    Route SMTP settings through Livewire

--- app-modules/settings/routes/web.php
+++ app-modules/settings/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Settings\Presentation\Http\Controllers\SmtpSettingsController;
 
 Route::middleware(['web', 'auth', 'permission:settings.manage'])->group(function (): void {
-    Route::get('/settings/smtp', [SmtpSettingsController::class, 'edit'])->name('settings.smtp.edit');
-    Route::put('/settings/smtp', [SmtpSettingsController::class, 'update'])->name('settings.smtp.update');
+    Route::livewire('/settings/smtp', 'settings::smtp')->name('settings.smtp.edit');
 });
