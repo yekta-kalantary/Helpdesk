c5e30c8d4d5e417be626cba00d6c26c72684cdd6
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 12:49:47 +0330

    register tasks module

---
+++ app-modules/tasks/src/TasksServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Tasks;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Tasks\Domain\Contracts\TaskAttachmentStore;
+use Modules\Tasks\Domain\Contracts\TaskNotifier;
+use Modules\Tasks\Domain\Contracts\TaskRepository;
+use Modules\Tasks\Infrastructure\DatabaseTaskNotifier;
+use Modules\Tasks\Infrastructure\EloquentTaskRepository;
+use Modules\Tasks\Infrastructure\LocalTaskAttachmentStore;
+
+class TasksServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(TaskRepository::class, EloquentTaskRepository::class);
+        $this->app->bind(TaskAttachmentStore::class, LocalTaskAttachmentStore::class);
+        $this->app->bind(TaskNotifier::class, DatabaseTaskNotifier::class);
+    }
+
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'tasks');
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'tasks');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+}
