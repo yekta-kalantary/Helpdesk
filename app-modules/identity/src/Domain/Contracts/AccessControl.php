dcd47d1d1b7b089aab3f0c3432074f324d02dfeb
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-07 14:56:42 +0330

    Make permissions code-defined

--- app-modules/identity/src/Domain/Contracts/AccessControl.php
+++ app-modules/identity/src/Domain/Contracts/AccessControl.php
@@ -7,7 +7,7 @@ interface AccessControl
     /** @return array<int, array{id:int,name:string,permissions:array<int,string>,system:bool}> */
     public function roles(): array;
 
-    /** @return array<int, array{id:int,name:string}> */
+    /** @return array<int, array{id:int,name:string,module:string}> */
     public function permissions(): array;
 
     public function createRole(string $name, array $permissions): void;
@@ -15,8 +15,4 @@ interface AccessControl
     public function updateRole(int $roleId, string $name, array $permissions): void;
 
     public function deleteRole(int $roleId): void;
-
-    public function createPermission(string $name): void;
-
-    public function deletePermission(int $permissionId): void;
 }
