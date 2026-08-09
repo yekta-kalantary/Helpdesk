631ae7ca4c6ff75d4653ac42ee5620034e9ba09a
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 23:00:01 +0330

    Link user accounts to contacts

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,50 +12,50 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['person_id', 'password', 'is_active'])]
+#[Fillable(['contact_id', 'password', 'is_active'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, HasRoles, Notifiable;
 
-    public function person(): BelongsTo
+    public function contact(): BelongsTo
     {
-        return $this->belongsTo(Person::class);
+        return $this->belongsTo(Contact::class);
     }
 
     protected function name(): Attribute
     {
         return Attribute::make(
-            get: fn (): string => $this->person?->first_name ?? '',
+            get: fn (): string => $this->contact?->first_name ?? '',
         );
     }
 
     protected function lastName(): Attribute
     {
         return Attribute::make(
-            get: fn (): string => $this->person?->last_name ?? '',
+            get: fn (): string => $this->contact?->last_name ?? '',
         );
     }
 
     protected function email(): Attribute
     {
         return Attribute::make(
-            get: fn (): string => $this->person?->email ?? '',
+            get: fn (): string => $this->contact?->email ?? '',
         );
     }
 
     protected function mobile(): Attribute
     {
         return Attribute::make(
-            get: fn (): string => $this->person?->mobile ?? '',
+            get: fn (): string => $this->contact?->mobile ?? '',
         );
     }
 
     protected function fullName(): Attribute
     {
         return Attribute::make(
-            get: fn (): string => $this->person?->full_name ?? '',
+            get: fn (): string => $this->contact?->full_name ?? '',
         );
     }
 
