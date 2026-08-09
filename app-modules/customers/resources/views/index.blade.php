e0a72db5d20e5891d1aee22e21d60b611cc1fcc2
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 14:05:31 +0330

    Add icons to customer actions

--- app-modules/customers/resources/views/index.blade.php
+++ app-modules/customers/resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <x-ui.page-header :title="__('customers::messages.customers')">
         <x-slot:actions>
             @can('customers.create')
-                <x-ui.button :href="route('customers.create')" wire:navigate>{{ __('customers::messages.new_customer') }}</x-ui.button>
+                <x-ui.button :href="route('customers.create')" icon="fa-plus" wire:navigate>{{ __('customers::messages.new_customer') }}</x-ui.button>
             @endcan
         </x-slot:actions>
     </x-ui.page-header>
@@ -15,7 +15,7 @@
         <div class="min-w-0 flex-1">
             <x-ui.input name="q" :value="$q" wire:model.live.debounce.300ms="q" :placeholder="__('customers::messages.search_placeholder')" />
         </div>
-        <span class="pb-2 text-xs text-slate-500" wire:loading wire:target="q">{{ __('app.loading') }}</span>
+        <span class="pb-2 text-xs text-slate-500" wire:loading wire:target="q"><i class="fa-light fa-spinner-third fa-spin ml-1" aria-hidden="true"></i>{{ __('app.loading') }}</span>
     </x-ui.filter-bar>
 
     <x-ui.table wire:loading.class="opacity-60" wire:target="q">
@@ -38,7 +38,7 @@
                     <td>
                         <div class="flex flex-wrap gap-2">
                             @can('customers.update')
-                                <x-ui.button size="sm" variant="secondary" :href="route('customers.edit', $customer['id'])" wire:navigate>{{ __('app.edit') }}</x-ui.button>
+                                <x-ui.button size="sm" variant="secondary" :href="route('customers.edit', $customer['id'])" icon="fa-pen-to-square" wire:navigate>{{ __('app.edit') }}</x-ui.button>
                             @endcan
                         </div>
                     </td>
