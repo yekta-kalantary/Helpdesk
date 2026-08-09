b5e8f443eb63231e7809bc5dfbd63e59bb99a26e
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 14:07:32 +0330

    Add icon to logout action

--- app-modules/identity/resources/views/logout.blade.php
+++ app-modules/identity/resources/views/logout.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-ui.button class="w-full" variant="secondary" wire:click="logout" wire:loading.attr="disabled" wire:target="logout">
+    <x-ui.button class="w-full" variant="secondary" icon="fa-arrow-right-from-bracket" wire:click="logout" wire:loading.attr="disabled" wire:target="logout">
         <span wire:loading.remove wire:target="logout">{{ __('app.logout') }}</span>
         <span wire:loading wire:target="logout">{{ __('app.loading') }}</span>
     </x-ui.button>
