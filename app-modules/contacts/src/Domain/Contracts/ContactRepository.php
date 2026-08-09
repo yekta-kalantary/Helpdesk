89eb97c9479745d034f65743a9a56f5470ee38bc
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 23:01:05 +0330

    Add contact repository contract

---
+++ app-modules/contacts/src/Domain/Contracts/ContactRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Contacts\Domain\Contracts;
+
+interface ContactRepository
+{
+    /** @return array<int,array<string,mixed>> */
+    public function search(?string $term = null): array;
+
+    /** @return array<string,mixed> */
+    public function find(int $id): array;
+
+    public function save(?int $id, array $contactAttributes, array $account): int;
+}
