9cb735457392b7a2c9425b6cae329c69f35dd9e2
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 14:05:44 +0330

    Add icons to project actions

--- app-modules/projects/resources/views/index.blade.php
+++ app-modules/projects/resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <x-ui.page-header :title="__('projects::messages.projects')">
         <x-slot:actions>
             @can('projects.create')
-                <x-ui.button :href="route('projects.create')" wire:navigate>{{ __('projects::messages.new_project') }}</x-ui.button>
+                <x-ui.button :href="route('projects.create')" icon="fa-plus" wire:navigate>{{ __('projects::messages.new_project') }}</x-ui.button>
             @endcan
         </x-slot:actions>
     </x-ui.page-header>
@@ -15,7 +15,7 @@
         <div class="min-w-0 flex-1">
             <x-ui.input name="q" :value="$q" wire:model.live.debounce.300ms="q" :placeholder="__('projects::messages.search_placeholder')" />
         </div>
-        <span class="pb-2 text-xs text-slate-500" wire:loading wire:target="q">{{ __('app.loading') }}</span>
+        <span class="pb-2 text-xs text-slate-500" wire:loading wire:target="q"><i class="fa-light fa-spinner-third fa-spin ml-1" aria-hidden="true"></i>{{ __('app.loading') }}</span>
     </x-ui.filter-bar>
 
     <x-ui.table wire:loading.class="opacity-60" wire:target="q,delete">
@@ -42,18 +42,18 @@
                     <td>
                         <div class="flex flex-wrap gap-2">
                             @can('tasks.view')
-                                <x-ui.button size="sm" variant="secondary" :href="route('tasks.index', ['project' => $project['id']])" wire:navigate>{{ __('app.tasks') }}</x-ui.button>
+                                <x-ui.button size="sm" variant="secondary" :href="route('tasks.index', ['project' => $project['id']])" icon="fa-list-check" wire:navigate>{{ __('app.tasks') }}</x-ui.button>
                             @endcan
                             @if($project['category'] === 'customer')
                                 @can('tickets.view')
-                                    <x-ui.button size="sm" variant="secondary" :href="route('tickets.index', ['project' => $project['id']])" wire:navigate>{{ __('app.tickets') }}</x-ui.button>
+                                    <x-ui.button size="sm" variant="secondary" :href="route('tickets.index', ['project' => $project['id']])" icon="fa-ticket" wire:navigate>{{ __('app.tickets') }}</x-ui.button>
                                 @endcan
                             @endif
                             @can('projects.update')
-                                <x-ui.button size="sm" variant="secondary" :href="route('projects.edit', $project['id'])" wire:navigate>{{ __('app.edit') }}</x-ui.button>
+                                <x-ui.button size="sm" variant="secondary" :href="route('projects.edit', $project['id'])" icon="fa-pen-to-square" wire:navigate>{{ __('app.edit') }}</x-ui.button>
                             @endcan
                             @can('projects.delete')
-                                <x-ui.button size="sm" variant="danger" wire:click="delete({{ $project['id'] }})" wire:confirm="{{ __('app.confirm_delete') }}" wire:loading.attr="disabled" wire:target="delete({{ $project['id'] }})">{{ __('app.delete') }}</x-ui.button>
+                                <x-ui.button size="sm" variant="danger" icon="fa-trash" wire:click="delete({{ $project['id'] }})" wire:confirm="{{ __('app.confirm_delete') }}" wire:loading.attr="disabled" wire:target="delete({{ $project['id'] }})">{{ __('app.delete') }}</x-ui.button>
                             @endcan
                         </div>
                     </td>
