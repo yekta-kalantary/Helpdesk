5d07b8d8d428de3bfb50748906231d38155d6583
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 23:10:52 +0330

    Remove task assignment notifications

--- app-modules/tasks/src/Application/Actions/SaveTask.php
+++ app-modules/tasks/src/Application/Actions/SaveTask.php
@@ -3,7 +3,6 @@
 namespace Modules\Tasks\Application\Actions;
 
 use Modules\Tasks\Domain\Contracts\TaskAttachmentStore;
-use Modules\Tasks\Domain\Contracts\TaskNotifier;
 use Modules\Tasks\Domain\Contracts\TaskRepository;
 
 class SaveTask
@@ -11,7 +10,6 @@ class SaveTask
     public function __construct(
         private readonly TaskRepository $tasks,
         private readonly TaskAttachmentStore $attachments,
-        private readonly TaskNotifier $notifier,
     ) {}
 
     public function execute(?int $id, array $attributes, array $files = [], ?int $previousAssignee = null): int
@@ -25,11 +23,6 @@ class SaveTask
 
         if ($files !== []) {
             $this->attachments->add($taskId, $files);
-        }
-
-        $assignee = $attributes['assigned_to'] ?? null;
-        if ($assignee && $assignee !== $previousAssignee) {
-            $this->notifier->assigned((int) $assignee, $taskId, $attributes['title']);
         }
 
         return $taskId;
