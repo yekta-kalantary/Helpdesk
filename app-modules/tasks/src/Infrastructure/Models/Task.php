ebae127b60ab378c14b5ac524b4748b0b0354d90
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 23:08:57 +0330

    Remove portal visibility from task model

--- app-modules/tasks/src/Infrastructure/Models/Task.php
+++ app-modules/tasks/src/Infrastructure/Models/Task.php
@@ -19,7 +19,7 @@ class Task extends Model implements HasMedia
 
     protected $fillable = [
         'project_id', 'title', 'description', 'assigned_to', 'created_by', 'priority', 'status',
-        'is_customer_visible', 'due_at', 'estimated_minutes', 'spent_minutes',
+        'due_at', 'estimated_minutes', 'spent_minutes',
     ];
 
     protected function casts(): array
@@ -27,7 +27,6 @@ class Task extends Model implements HasMedia
         return [
             'priority' => TaskPriority::class,
             'status' => TaskStatus::class,
-            'is_customer_visible' => 'boolean',
             'due_at' => 'datetime',
             'estimated_minutes' => 'integer',
             'spent_minutes' => 'integer',
