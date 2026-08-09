bfeae094e2cdc71cafcb123646c0602b55b2cf4e
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-09 09:29:52 +0330

    Stack form actions on mobile

--- resources/views/components/ui/form-actions.blade.php
+++ resources/views/components/ui/form-actions.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->class(['flex flex-wrap items-center gap-2 border-t border-slate-100 pt-5']) }}>
+<div {{ $attributes->class(['flex flex-col gap-2 border-t border-slate-100 pt-5 sm:flex-row sm:flex-wrap sm:items-center [&>a]:w-full [&>button]:w-full [&>form]:w-full sm:[&>a]:w-auto sm:[&>button]:w-auto sm:[&>form]:w-auto [&>form>button]:w-full']) }}>
     {{ $slot }}
 </div>
