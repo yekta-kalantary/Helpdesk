5025f4937fe4a6d78455b522970cff1d073162db
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-10 22:34:42 +0330

    Remove project repository binding

--- app-modules/projects/src/ProjectsServiceProvider.php
+++ app-modules/projects/src/ProjectsServiceProvider.php
@@ -4,16 +4,9 @@ namespace Modules\Projects;
 
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
-use Modules\Projects\Domain\Contracts\ProjectRepository;
-use Modules\Projects\Infrastructure\EloquentProjectRepository;
 
 class ProjectsServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        $this->app->bind(ProjectRepository::class, EloquentProjectRepository::class);
-    }
-
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
