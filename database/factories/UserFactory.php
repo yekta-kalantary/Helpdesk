ca668df854527aabbccd1b337ad7c4426a35828a
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-10 22:10:55 +0330

    Update user factory for simple schema

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Modules\Contacts\Infrastructure\Models\Contact;
 use Modules\Identity\Infrastructure\Models\User;
 
 /**
@@ -20,18 +19,25 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'contact_id' => Contact::factory(),
+            'name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email' => fake()->unique()->safeEmail(),
+            'mobile' => fake()->optional()->numerify('09#########'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'is_active' => true,
+            'is_admin' => false,
             'remember_token' => Str::random(10),
         ];
     }
 
+    public function admin(): static
+    {
+        return $this->state(fn () => ['is_admin' => true]);
+    }
+
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(fn () => ['email_verified_at' => null]);
     }
 }
