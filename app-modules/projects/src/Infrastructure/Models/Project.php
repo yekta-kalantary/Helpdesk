5709bbfaf8431f28e36c126a677da93547789c9f
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-10 22:06:05 +0330

    Keep project model dependency minimal

--- app-modules/projects/src/Infrastructure/Models/Project.php
+++ app-modules/projects/src/Infrastructure/Models/Project.php
@@ -4,9 +4,7 @@ namespace Modules\Projects\Infrastructure\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Modules\Identity\Infrastructure\Models\User;
-use Modules\Tasks\Infrastructure\Models\Task;
 
 class Project extends Model
 {
@@ -18,10 +16,5 @@ class Project extends Model
     public function members(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'project_user')->withTimestamps();
-    }
-
-    public function tasks(): HasMany
-    {
-        return $this->hasMany(Task::class);
     }
 }
