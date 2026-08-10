f0edb9c360947dc36df178ab3aa07f33e90af267
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-10 22:38:32 +0330

    Remove unused authorization gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,19 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-use Modules\Identity\Infrastructure\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
-
-    public function boot(): void
-    {
-        Gate::define('admin', static fn (User $user): bool => (bool) $user->is_admin);
-    }
 }
