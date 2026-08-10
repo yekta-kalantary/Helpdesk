f657ea30b8530a2b9031dad2e5b000239839b816
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-10 22:09:35 +0330

    Remove task permissions and attachments routes

--- app-modules/tasks/routes/web.php
+++ app-modules/tasks/routes/web.php
@@ -1,26 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Tasks\Presentation\Http\Controllers\TaskAttachmentController;
 
 Route::middleware(['web', 'auth'])->group(function (): void {
-    Route::livewire('/tasks', 'tasks::index')
-        ->middleware('permission:tasks.view')
-        ->name('tasks.index');
-
-    Route::livewire('/tasks/create', 'tasks::form')
-        ->middleware('permission:tasks.create')
-        ->name('tasks.create');
-
-    Route::livewire('/tasks/{task}', 'tasks::show')
-        ->middleware('permission:tasks.view')
-        ->name('tasks.show');
-
-    Route::livewire('/tasks/{task}/edit', 'tasks::form')
-        ->middleware('permission:tasks.update')
-        ->name('tasks.edit');
-
-    Route::get('/tasks/{task}/attachments/{media}', TaskAttachmentController::class)
-        ->middleware('permission:tasks.view')
-        ->name('tasks.attachments.download');
+    Route::livewire('/tasks', 'tasks::index')->name('tasks.index');
+    Route::livewire('/tasks/create', 'tasks::form')->name('tasks.create');
+    Route::livewire('/tasks/{task}/edit', 'tasks::form')->name('tasks.edit');
 });
