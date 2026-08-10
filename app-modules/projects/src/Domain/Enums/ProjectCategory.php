d23e695839883c2e09a12fb93ee4cce3d879b909
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-10 22:25:43 +0330

    Remove legacy modules and dead code

--- app-modules/projects/src/Domain/Enums/ProjectCategory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Projects\Domain\Enums;
-
-enum ProjectCategory: string
-{
-    case Contact = 'contact';
-    case Internal = 'internal';
-}
--- app-modules/tasks/src/Domain/Contracts/TaskAttachmentReader.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Tasks\Domain\Contracts;
-
-interface TaskAttachmentReader
-{
-    /** @return array{name:string,path:string,mime_type:?string} */
-    public function get(int $taskId, int $mediaId): array;
-}
