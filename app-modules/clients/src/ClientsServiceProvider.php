7e3ccd4d6a2a41f11affb66e21dcaecd7fc4def0
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-11 14:15:30 +0330

    feat: boot client management UI

--- app-modules/clients/src/ClientsServiceProvider.php
+++ app-modules/clients/src/ClientsServiceProvider.php
@@ -3,11 +3,21 @@
 namespace Modules\Clients;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class ClientsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'clients');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        Livewire::addNamespace(
+            namespace: 'clients',
+            classNamespace: 'Modules\\Clients\\Presentation\\Livewire',
+            classPath: __DIR__.'/Presentation/Livewire',
+            classViewPath: __DIR__.'/../resources/views',
+        );
     }
 }
