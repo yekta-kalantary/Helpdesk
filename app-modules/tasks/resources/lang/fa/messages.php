65d688f58a80a19c60fc17cde68a8f2e758be69c
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-11 14:34:39 +0330

    feat: add task collaboration messages

--- app-modules/tasks/resources/lang/fa/messages.php
+++ app-modules/tasks/resources/lang/fa/messages.php
@@ -5,5 +5,7 @@ return [
     'new_task' => 'تسک جدید',
     'edit_task' => 'ویرایش تسک',
     'project' => 'پروژه',
-    'search_placeholder' => 'جستجو در عنوان تسک',
+    'search_placeholder' => 'جستجو در عنوان یا Reference تسک',
+    'comment_or_attachment_required' => 'برای ثبت نظر، متن یا حداقل یک فایل لازم است.',
+    'too_many_uploads' => 'تعداد آپلودها بیش از حد مجاز است. کمی بعد دوباره تلاش کنید.',
 ];
