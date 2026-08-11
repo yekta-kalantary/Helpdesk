80d74a426576506c0d62439e8aeabe7d1a551801
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-11 14:10:09 +0330

    feat: add authorized attachment route

--- app-modules/tasks/routes/web.php
+++ app-modules/tasks/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Tasks\Presentation\Http\Controllers\AttachmentDownloadController;
 
 Route::middleware(['web', 'auth'])->group(function (): void {
+    Route::get('/attachments/{attachment}', AttachmentDownloadController::class)->name('attachments.download');
+
     Route::livewire('/tasks', 'tasks::index')->name('tasks.index');
     Route::livewire('/tasks/create', 'tasks::form')->name('tasks.create');
     Route::livewire('/tasks/{task}/edit', 'tasks::form')->name('tasks.edit');
