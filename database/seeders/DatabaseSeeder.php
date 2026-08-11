0167a86b17191b49a45bde01959e480e4e7cee8c
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-11 14:00:54 +0330

    feat: seed immutable admin role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Identity\Domain\Enums\UserRole;
 use Modules\Identity\Infrastructure\Models\User;
 
 class DatabaseSeeder extends Seeder
@@ -12,12 +13,13 @@ class DatabaseSeeder extends Seeder
         User::query()->updateOrCreate(
             ['email' => config('helpdesk.admin.email')],
             [
+                'client_id' => null,
+                'role' => UserRole::Admin,
                 'name' => config('helpdesk.admin.first_name'),
                 'last_name' => config('helpdesk.admin.last_name'),
                 'mobile' => config('helpdesk.admin.mobile'),
                 'password' => config('helpdesk.admin.password'),
                 'is_active' => true,
-                'is_admin' => true,
             ],
         );
     }
