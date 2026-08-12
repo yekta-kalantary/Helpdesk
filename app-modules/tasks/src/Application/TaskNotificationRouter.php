69967bd17d0f454e00cb0d8df7599fdbda1dbdbb
Ali (Yekta) Rezaei Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2026-08-12 22:30:15 +0330

    refactor: centralize task notification recipients

--- app-modules/tasks/src/Application/TaskNotificationRouter.php
+++ app-modules/tasks/src/Application/TaskNotificationRouter.php
@@ -4,6 +4,7 @@ namespace Modules\Tasks\Application;
 
 use Illuminate\Support\Collection;
 use Modules\Identity\Infrastructure\Models\User;
+use Modules\Tasks\Domain\Enums\TaskStatus;
 use Modules\Tasks\Infrastructure\Models\Task;
 
 class TaskNotificationRouter
@@ -13,11 +14,49 @@ class TaskNotificationRouter
     {
         $task->loadMissing('assignee');
 
-        return collect([$task->assignee])->filter()->values();
+        if ($task->assignee) {
+            return collect([$task->assignee]);
+        }
+
+        return $this->isUnassignedAdminQueue($task)
+            ? $this->adminQueue()
+            : collect();
+    }
+
+    /** @return Collection<int, User> */
+    public function assigneeChanged(Task $task): Collection
+    {
+        $task->loadMissing('assignee');
+
+        if ($task->assignee) {
+            return collect([$task->assignee]);
+        }
+
+        return $this->isUnassignedAdminQueue($task)
+            ? $this->adminQueue()
+            : collect();
     }
 
     /** @return Collection<int, User> */
     public function statusChanged(Task $task): Collection
+    {
+        return $this->withAdminQueueFallback($task, $this->taskAudience($task));
+    }
+
+    /** @return Collection<int, User> */
+    public function commentAdded(Task $task): Collection
+    {
+        return $this->withAdminQueueFallback($task, $this->taskAudience($task));
+    }
+
+    /** @return Collection<int, User> */
+    public function adminQueue(): Collection
+    {
+        return User::query()->active()->admins()->get();
+    }
+
+    /** @return Collection<int, User> */
+    private function taskAudience(Task $task): Collection
     {
         $task->loadMissing(['creator', 'assignee']);
 
@@ -25,5 +64,25 @@ class TaskNotificationRouter
             ->filter()
             ->unique('id')
             ->values();
+    }
+
+    /** @param Collection<int, User> $recipients
+     *  @return Collection<int, User>
+     */
+    private function withAdminQueueFallback(Task $task, Collection $recipients): Collection
+    {
+        if (! $this->isUnassignedAdminQueue($task)) {
+            return $recipients;
+        }
+
+        return $recipients
+            ->merge($this->adminQueue())
+            ->unique('id')
+            ->values();
+    }
+
+    private function isUnassignedAdminQueue(Task $task): bool
+    {
+        return $task->status === TaskStatus::WaitingAdmin && $task->assigned_to === null;
     }
 }
